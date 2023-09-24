495626e030b230362b84b1ac878a1245117fb714
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2023-09-24 07:16:29 +0000

    updated task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,10 +9,11 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int tmp;
+	int tmp, flag;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		flag = 0;
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -21,7 +22,10 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 				print_array(array, size);
+				flag = 1;
 			}
 		}
+		if (flag == 0)
+			break;
 	}
 }
